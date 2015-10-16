be4020ceab5644d37ab88d0983b6213a69d95db5
rizwanhkm <rizwanhkm@gmail.com>
2015-10-16 21:15:55 +0530

    added more settings

--- setup.js
+++ setup.js
@@ -2,3 +2,20 @@ var userGuid = "";
 var apiKey = "";
 var maxCharCountperCell = 32750;
 var timegap=100;
+
+$(document).ready( function(){
+    $("#charpercell").val(maxCharCountperCell);
+    $("#timegap").val(timegap);
+
+});
+
+$("#charpercell").on("change", function(){
+  maxCharCountperCell = $("#charpercell").val();
+  window.console.log("MaxCountPerCell = ", maxCharCountperCell);
+});
+
+$("#timegap").on("change", function(){
+  timegap = $("#timegap").val();
+  window.console.log("Timegap = ", timegap);
+
+});
